e331a73d350ec70a700ac6e60ed71664ce63f4ba
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-12-13 17:17:28 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 24, "male", "brown", "angry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 38, "male", "pink", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha",12, "female", "black", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 11, "male", "green", "moderate", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 31, "female", "blue", "harsh", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 20, "male", "orange", "playful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 53, "female", "pale", "moderate", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 49, "male", "white", "angry", 0);
