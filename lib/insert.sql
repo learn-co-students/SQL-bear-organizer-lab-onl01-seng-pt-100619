6696932a621359ae7966a6d799ac302f61b8985b
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-12-14 01:55:35 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,17 @@
-INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUE (1,"Mr. Chocolate", 3, 'female', 'brown', 'calm', 0);
 
-INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUE (2,"Rowdy", 3, 'female', 'brown', 'calm', 0);
+INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (1,"Mr. Chocolate", 3, 'female', 'brown', 'calm', 0);
 
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUE (3,'Tabitha', 3, 'female', 'brown', 'calm', 0);
+INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (2,"Rowdy", 3, 'female', 'brown', 'calm', 0);
 
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUE (4,"Sargeant Brown", 3, 'female', 'brown', 'calm', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3,'Tabitha', 3, 'female', 'brown', 'calm', 0);
 
-INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUE (5,'Melissa', 3, 'female', 'brown', 'calm', 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4,"Sargeant Brown", 3, 'female', 'brown', 'calm', 0);
 
-INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUE (6,'Grinch', 3, 'female', 'brown', 'calm', 0);
+INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (5,'Melissa', 3, 'female', 'brown', 'calm', 0);
 
-INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUE (7,'Wendy', 3, 'female', 'brown', 'calm', 0);
+INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (6,'Grinch', 3, 'female', 'brown', 'calm', 0);
 
-INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUE (id,NULL, 3, 'female', 'brown', 'calm', 0);
+INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (7,'Wendy', 3, 'female', 'brown', 'calm', 0);
+
+INSERT INTO bears (id,name, age, gender, color, temperament, alive) VALUES (8,NULL, 3, 'female', 'brown', 'calm', 0);
+
